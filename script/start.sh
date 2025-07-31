8d4e28c9740eabfe15361709c8085b8e2fdbb55e
ZhenYi <434836402@qq.com>
2025-07-31 09:29:26 +0800

    feat(repo): Optimize warehouse file path display and navigation

--- script/start.sh
+++ script/start.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-function start_jzfs_git() {
-  echo "Starting jzfs_git..."
-  sh -c /explore/jzfs & > /dev/null 2>&1
-}
-
-function start_nginx() {
-  echo "Starting nginx..."
-  nginx -g "daemon off;"
-}
-
-start_jzfs_git()
-start_nginx()
+echo "Starting jzfs_git..."
+sh -c /explore/jzfs & > /dev/null 2>&1
+echo "Starting nginx..."
+nginx -g "daemon off;"
